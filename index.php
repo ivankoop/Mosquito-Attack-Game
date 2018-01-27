48d4688a5cb2b73bd8867b8071c09e64a1a6658a
ivan koop <ivankoop3@gmail.com>
2018-01-26 21:56:01 -0300

    primera noche

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>GLOBAL GAME JAM 2018</title>
+    <title>GLOBAL GAME JAM 2018 ASUNCIÓN PARAGUAY</title>
     <script type="text/javascript" src="system/phaser.min.js"></script>
     <script type="text/javascript" src="states/main.js"></script>
   </head>
   <body>
-    <h1>GLOBAL GAME JAM 2018</h1>
+    <h1>GLOBAL GAME JAM 2018 ASUNCIÓN PARAGUAY</h1>
 
     <script type="text/javascript">
       var game = new Phaser.Game(800, 600, Phaser.AUTO, 'ggj-2018', { preload: preload, create: create, update: update });
@@ -15,6 +15,10 @@
       game.state.add("main",main);
 
       function preload() {
+        game.load.image('mosquito', 'assets/mosquito.png');
+        game.load.image('people', 'assets/people.png');
+        game.load.image('bucket', 'assets/bucket.png');
+        game.load.image('background', 'assets/background.jpg');
         console.log("preload del game");
 
       }
--- states/main.js
+++ states/main.js
@@ -5,17 +5,120 @@ var main = function(game){
 
 main.prototype = {
 
-  //local attrs here
+
+	//MOSQUITO VARIABLES HERE BEGIN
+	mosquito_x_max_speed: 5,
+	mosquito_x_min_speed: -5,
+
+	mosquito_x_speed: 0,
+
+	mosquito_y_max_speed: 5,
+	mosquito_y_min_speed: -5,
+
+	mosquito_y_speed: 0,
+	mosquito: null,
+	//MOSQUITO VARIABLES HERE END
+
+	people: null,
+
+	bucket: null,
+
+	score_text: null,
+	score: 0,
 
   preload: function() {
     console.log("preload main");
+
   },
 
   create: function() {
-    console.log("create del main");
+
+		game.add.sprite(-150,0,'background');
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		this.mosquito = game.add.sprite(100, 400, 'mosquito');
+		this.mosquito.anchor.set(0.5);
+		game.physics.enable(this.mosquito, Phaser.Physics.ARCADE);
+		this.mosquito.body.collideWorldBounds = true;
+
+		this.mosquito.body.bounce.setTo(1, 1);
+
+		this.people = game.add.sprite(650,340, 'people');
+		game.physics.enable(this.people, Phaser.Physics.ARCADE);
+		this.people.body.collideWorldBounds = true;
+		this.people.body.immovable = true;
+
+		this.bucket = game.add.sprite(0,450, 'bucket');
+		game.physics.enable(this.bucket, Phaser.Physics.ARCADE);
+
+		this.score_text = game.add.text(20,20, 'Score: ' + this.score);
+
   },
 
   update: function() {
-    console.log("update del main");
-  }
+
+		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+
+			if(this.mosquito_x_speed > this.mosquito_x_min_speed) {
+				this.mosquito_x_speed -= 0.2;
+			}
+
+
+		} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+
+			if(this.mosquito_x_speed < this.mosquito_x_max_speed) {
+				this.mosquito_x_speed += 0.2;
+			}
+
+
+		}
+
+		if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+
+			if(this.mosquito_y_speed < this.mosquito_y_max_speed) {
+				this.mosquito_y_speed += 0.2;
+			}
+
+		} else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+
+			if(this.mosquito_y_speed > this.mosquito_y_min_speed) {
+				this.mosquito_y_speed -= 0.2;
+			}
+
+		}
+
+		this.mosquito.y += this.mosquito_y_speed;
+		this.mosquito.x += this.mosquito_x_speed;
+
+		game.physics.arcade.overlap(this.mosquito, this.people, this._collisionPeople, null, this);
+		game.physics.arcade.overlap(this.mosquito, this.bucket, this._collisionBucket, null, this);
+
+  },
+
+	_collisionPeople: function(mosquito, people) {
+		console.log("collision con people");
+		mosquito.x -= 10;
+
+		this.mosquito_x_speed = 0;
+		this.mosquito_y_speed = 0;
+	},
+
+	_collisionBucket: function(mosquito, bucket) {
+		console.log("collision con bucket");
+
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+			console.log("esta dejando las larvas");
+			this.score += 10;
+			this.score_text.setText('Score: ' + this.score);
+		}
+
+	}
+
+	/*_collectDesalojo: function(player, desalojo){
+
+		this.collect_sound.play();
+		desalojo.kill();
+
+	},*/
 }
